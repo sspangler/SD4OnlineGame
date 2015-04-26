a124ada57be4766bbcc3d2f7c1cb824ffefeb6fc
RickySanders <rsanders@ltu.edu>
2015-04-26 11:26:16 -0400

    Organized scripts; added early base for game ui

---
+++ SD4_2DOnlineGame/Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimations : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ SD4_2DOnlineGame/Assets/Scripts/UI/UI_GameMode.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_GameMode : MonoBehaviour {
+
+	public Text [] statDisplay;
+
+	void Awake () {
+		//Get Text components if not already set
+		if (statDisplay == null && 
+		    GetComponentsInChildren<Text>() != null)
+			statDisplay = GetComponentsInChildren<Text>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
