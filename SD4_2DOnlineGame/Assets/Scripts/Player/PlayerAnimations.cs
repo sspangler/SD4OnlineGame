10720637203991aca5edd153129fffb3f44fc433
RickySanders <rsanders@ltu.edu>
2015-04-27 08:49:03 -0400

    Implementation of wizard class animations

--- SD4_2DOnlineGame/Assets/Scripts/Player/PlayerAnimations.cs
+++ SD4_2DOnlineGame/Assets/Scripts/Player/PlayerAnimations.cs
@@ -3,6 +3,22 @@ using System.Collections;
 
 public class PlayerAnimations : MonoBehaviour {
 
+    /// <summary>
+    /// animDir - value used to control the direction of the player's on-screen images
+    /// 2 = back (north)
+    /// -1, 1 = side (west, east)
+    /// 0 = front (south)
+    /// </summary>
+    int animDir;
+
+    Animator anim;
+
+    void Awake() {
+        //Gets Animator component
+        if (GetComponent<Animator>() != null)
+            anim = GetComponent<Animator>();
+    }
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +26,19 @@ public class PlayerAnimations : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        spriteDirection();
 	}
+
+    void spriteDirection() {
+        //Get the current scale of the transform
+        Vector3 currScale = transform.localScale;
+
+        //If player is facing left or right,
+        //set the X-scale to face proper direction based on animDir
+        if (animDir ==- 1 || animDir == 1) 
+            currScale.x *= animDir;
+
+        //Set the scale of the player transform to the altered currScale
+        transform.localScale = currScale;
+    }
 }
--- SD4_2DOnlineGame/Assets/Scripts/UI/UI_GameMode.cs
+++ SD4_2DOnlineGame/Assets/Scripts/UI/UI_GameMode.cs
@@ -3,14 +3,20 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class UI_GameMode : MonoBehaviour {
-
+	
 	public Text [] statDisplay;
+    public Button hpBar;
 
 	void Awake () {
 		//Get Text components if not already set
 		if (statDisplay == null && 
 		    GetComponentsInChildren<Text>() != null)
 			statDisplay = GetComponentsInChildren<Text>();
+
+        //Get HP bar if not already set
+        if (hpBar == null &&
+            GetComponentsInChildren<Button>() != null)
+            hpBar = GetComponentInChildren<Button>();
 	}
 
 	// Use this for initialization
@@ -20,6 +26,10 @@ public class UI_GameMode : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
+
+    void displayPlayerStats() {
+
+    }
 }
