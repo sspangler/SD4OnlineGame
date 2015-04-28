3ff43a6c1dc1a961d995d903d558f1ef82ed0978
sspangler <sspangler@ltu.edu>
2015-04-28 19:44:25 -0400

    Stealth Boss and a Flee_Charge Boss

---
+++ SD4_2DOnlineGame/Assets/BossStealth.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossStealth : MonoBehaviour {
+
+	public EnemyStats enemyStats;
+	
+	Vector3 target;
+	float speed;
+
+	bool stalk;
+	float timeToAttack = 0;
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		enemyStats = GetComponent<EnemyStats> ();
+		speed = enemyStats.moveSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (stalk) {
+			timeToAttack -= Time.deltaTime;
+			if (timeToAttack <= 0)
+			{
+
+			}
+		}
+
+	}
+
+	void Attack () {
+		transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+	}
+
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "Player") {
+			stalk = true;
+			player = col.gameObject;
+		}
+	}
+}
---
+++ SD4_2DOnlineGame/Assets/Charge_Flee.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Charge_Flee : MonoBehaviour {
+
+	EnemyStats enemyStats;
+	EnemyCharge enemyCharge;
+	EnemyFlee enemyFlee;
+
+	// Use this for initialization
+	void Start () {
+		enemyStats = GetComponent<EnemyStats>();
+		enemyCharge = GetComponent<EnemyCharge>();
+		enemyFlee = GetComponent<EnemyFlee>();
+		enemyFlee.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (enemyStats.health <= 2)
+		{
+			enemyStats.health += enemyStats.healthRegen * Time.deltaTime;
+			enemyCharge.enabled = false;
+			enemyFlee.enabled = true;
+		}
+		else if ( enemyStats.health >= 10)
+		{
+			enemyCharge.enabled = true;
+			enemyFlee.enabled = false;
+		}
+
+	}
+}
