9e28b43a8ecf727b5b9b57aa3ccdac2b42db3920
ANGRyYb0b <kkleber@ltu.edu>
2015-04-29 13:16:38 -0400

    reverted to ricky's update and then added the ability for the player to shoot

--- SD4_2DOnlineGame/Assets/Scripts/Player/playercontroller.cs
+++ SD4_2DOnlineGame/Assets/Scripts/Player/playercontroller.cs
@@ -3,45 +3,69 @@ using System.Collections;
 
 public class playercontroller : MonoBehaviour {
 
-    public float health, currHealth;
-    public float healthRegen;
-    public float moveSpeed;
-    public float Attack;
-    public float attackSpeed;
-    public float defense;
-
-    public bool isMoving, isAttacking;
-	public GameObject bullet;
+    public float level, currEXP, nextLevelEXP;
+    public float currVitality, vitality, healthRegen;
+    public float power, atkSpd, def, moveSpd;
 	public float tempTime;
+	
+	public GameObject bullet;
+
+    public bool isMoving;
+
+    /*
+     * Important info:
+     * iarray - array that holds character stats
+     *          index 1 = level
+     *          index 2 = experience?
+     *          index 3 = vitality
+     *          index 4 = power
+     *          index 5 = atk speed
+     *          index 6 = defense
+     *          index 7 = move speed
+     **/
+    usave_file charSave;
+
+    void Awake() {
+        //Get character save information
+        if (GameObject.Find("CharSave").GetComponent<usave_file>() != null) {
+            charSave = GameObject.Find("CharSave").GetComponent<usave_file>();
+        }
+    }
 
 	// Use this for initialization
 	void Start () {
-
-	
-
-	
+	    //Read character stats from save file
+        level = charSave.iarray[1];
+        currEXP = charSave.iarray[2];
+        nextLevelEXP = Mathf.Pow(level, 2f);    //Change to represent experience needed for current level
+        vitality = charSave.iarray[3];
+        currVitality = vitality;
+        power = charSave.iarray[4];
+        atkSpd = charSave.iarray[5];
+        def = charSave.iarray[6];
+        moveSpd = charSave.iarray[7];
 	}
 	
 	// Update is called once per frame
 	void Update () {
         MovePlayer();
+        ValueCorrection();
 
+
+		
 		tempTime += Time.deltaTime;
-
-		if (tempTime >= attackSpeed)
+		
+		if (tempTime >= atkSpd)
 		{
 			if (Input.GetMouseButton (0)) 
-		
+				
 			{
-			
+				
 				GameObject clone = (GameObject) Instantiate (bullet, transform.position, Quaternion.identity);
-
+				
 				tempTime = 0;
 			}
 		}
-
-
-
 
 
 
@@ -74,26 +98,28 @@ public class playercontroller : MonoBehaviour {
         //    isMoving = true;
         //}
 
+        //Read horizontal input (-1 for left, 1 for right, 0 for none)
         float horizontal = Input.GetAxis("Horizontal");
+
+        ///Read vertical input (-1 for down, 1 for up, 0 for none)
         float vertical = Input.GetAxis("Vertical");
 
+        //If either horizontal or vertical movement is detected,
+        //then the player is moving.
+        //Otherwise, reset moving status
         if (horizontal != 0f || vertical != 0f)
             isMoving = true;
         else if (horizontal == 0f && vertical == 0f)
             isMoving = false;
 
-        transform.position += new Vector3(horizontal * moveSpeed * Time.deltaTime,
-                                          vertical * moveSpeed * Time.deltaTime,
+        //Move player transform an amount equal to moveSpeed in respective directions
+        transform.position += new Vector3(horizontal * (moveSpd / 1) * Time.deltaTime,
+                                          vertical * (moveSpd / 1) * Time.deltaTime,
                                           0);
     }
 
-    //void ActivateAttack() {
-    //    bool atk = Input.GetMouseButtonDown(0);
-
-    //    if (!isAttacking && atk) isAttacking = true;
-    //}
-
-    //public void endAttack() {
-    //    isAttacking = false;
-    //}
+    void ValueCorrection() {
+        Mathf.Clamp(currVitality, 0, vitality);
+        Mathf.Clamp(currEXP, 0, nextLevelEXP);
+    }
 }
--- SD4_2DOnlineGame/Assets/playerBullet.cs
+++ SD4_2DOnlineGame/Assets/playerBullet.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class playerBullet : MonoBehaviour {
 	
-
+	
 	float liveTime = 1.3f;
 	Vector3 target;
 	float angle;
@@ -12,47 +12,47 @@ public class playerBullet : MonoBehaviour {
 	Vector3 dir;
 	Vector3 sp;
 	bool adjust = true;
-
-		
+	
+	
 	void Start () {
-			
+		
 		target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		direction = (target - transform.position);
 		GameObject player = GameObject.Find ("Player");	
 		playercontroller playerScript = player.GetComponent<playercontroller>();
-		damage = playerScript.Attack;
-
-		 
-
-
-		}
+		damage = playerScript.power;
 		
 		
+		
+		
+	}
+	
+	
 	void Update () {
-			
-			if (adjust == true) {
+		
+		if (adjust == true) {
 			sp = Camera.main.WorldToScreenPoint (transform.position);
 			dir = (Input.mousePosition - sp).normalized;
 			GetComponent<Rigidbody2D> ().AddForce (dir * 800);
-
+			
 			adjust = false;
 		}
-
-			
-			
-			liveTime -= Time.deltaTime;
-			if (liveTime <= 0)
-				Destroy (gameObject);
-			
-
-				
-			
-
-			//transform.position = Vector3.MoveTowards(transform.position, target, 18 * Time.deltaTime); // change13 to attack speed of player and time related to distance
-
-			
-			
-			
-			
-		}
+		
+		
+		
+		liveTime -= Time.deltaTime;
+		if (liveTime <= 0)
+			Destroy (gameObject);
+		
+		
+		
+		
+		
+		//transform.position = Vector3.MoveTowards(transform.position, target, 18 * Time.deltaTime); // change13 to attack speed of player and time related to distance
+		
+		
+		
+		
+		
 	}
+}
